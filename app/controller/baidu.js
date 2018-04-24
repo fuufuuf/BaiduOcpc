bfcbf4253e04d921781d21c4a0cff2b48c4719ca
fuufuuf <flywangh@gmail.com>
2018-04-24 18:04:00 +0800

    updates ocpc

--- app/controller/baidu.js
+++ app/controller/baidu.js
@@ -8,10 +8,10 @@ class BaiduController extends Controller {
     callback_url = callback_url.replace('{{ATYPE}}', 'activate').replace('{{AVALUE}}', 0);
     const akey = 'MjU0Nzk0NzI=';
     callback_url += akey;
-    const sign = crypto.createHash('md5').update(callback_url);
+    const sign = crypto.createHash('md5').update(callback_url).digest('hex');
     callback_url += '&'+sign;
     console.log(callback_url);
-    const ret = ctx.app.curl(callback_url);
+    const ret = await ctx.app.curl(callback_url);
     console.log(ret);
     ctx.status = 200;
 
