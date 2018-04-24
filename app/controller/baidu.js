621c91c0f3a8d0ce8db3ea177b0109556cd6c0f4
fuufuuf <flywangh@gmail.com>
2018-04-24 17:47:55 +0800

    updates

--- app/controller/baidu.js
+++ app/controller/baidu.js
@@ -1,10 +1,18 @@
 const Controller = require('egg').Controller;
-
+const crypto = require('Crypto');
 class BaiduController extends Controller {
   async ocpc() {
-    const ctx = this.ctx;
+    const ctx = this.ctx; 
     console.log(ctx.query);
-    console.log(ctx.href);
+    let callback_url = ctx.query.callback_url;
+    callback_url = callback_url.replace('{{ATYPE}}', 'activate').replace('{{AVALUE}}', 0);
+    const akey = 'MjU0Nzk0NzI=';
+    callback_url += akey;
+    const sign = crypto.createHash('md5').update(callback_url);
+    callback_url += '&'+sign;
+    console.log(callback_url);
+    const ret = ctx.app.curl(callback_url);
+    console.log(ret);
     ctx.status = 200;
 
   }
--- test.js
+++ test.js
@@ -1,10 +1,4 @@
-let jsonData = {
-    id: {流水: 123},
-    status: "OK",
-    data: [867, 5309]
-  };
-  
-  let { id, status, data:number } = jsonData;
-  
-  console.log(id, status, number);
+let a = "xyz{{atype}}"
+let tmp = a.replace("{{atype}}",'activate');
+console.log(tmp);
   // 42, "OK", [867, 5309]
