e07de02513cf496bb5bcfd3b33116db42813e083
fuufuuf <flywangh@gmail.com>
2018-04-24 16:25:43 +0800

    updates

--- app/router.js
+++ app/router.js
@@ -1,6 +1,8 @@
 module.exports = app => {
     const { router, controller } = app;
     router.get('/', controller.home.index);
-    router.get('/ocpc', controller.baidu.ocpc);
+    router.get('/ocpc/click', controller.baidu.ocpc);
+    router.get('/ocpc/result', controller.baidu.ocpc);
+
   }
   ;
