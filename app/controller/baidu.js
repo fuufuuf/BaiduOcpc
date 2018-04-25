ef0b853fe73d399e57154580bc45b5ec290cf70b
fuufuuf <flywangh@gmail.com>
2018-04-25 16:10:07 +0800

    change md5 url

--- app/controller/baidu.js
+++ app/controller/baidu.js
@@ -7,9 +7,9 @@ class BaiduController extends Controller {
     let callback_url = ctx.query.callback_url;
     callback_url = callback_url.replace('{{ATYPE}}', 'activate').replace('{{AVALUE}}', 0);
     const akey = 'MjU0Nzk0NzI=';
-    callback_url += akey;
-    const sign = crypto.createHash('md5').update(callback_url).digest('hex');
-    callback_url += '&'+sign;
+    const md5Url = callback_url + akey;
+    const sign = crypto.createHash('md5').update(md5Url).digest('hex');
+    callback_url += '&sign='+sign;
     console.log(callback_url);
     // const ret = await ctx.app.curl(callback_url);
     // console.log(ret);
